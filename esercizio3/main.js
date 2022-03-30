403a2bd2569bb64c5681ab72a99750f54d27f2f2
Rostrv <trovatorosario91@gmail.com>
2022-03-30 11:38:35 +0200

    esercizio3

---
+++ esercizio3/main.js
@@ -0,0 +1,12 @@
+/*Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.*/
+
+let somma = 0
+
+//let numero = parseInt(prompt('inserisci un numero'))
+
+for (let i = 0; i < 10; i++) {
+    let numero = parseInt(prompt('inserisci un numero'))
+    somma = somma + numero;
+}
+console.log(somma)
