66cb7fa4278c3b6474bda05d3770a6df117ccc25
Rostrv <trovatorosario91@gmail.com>
2022-03-30 14:45:13 +0200

    minor corrections

--- esecizio2/main.js
+++ esecizio2/main.js
@@ -2,16 +2,18 @@
 Il software stampa prima la parola più corta, poi la parola più lunga.*/
 
 let firstWord = prompt('inserisci una parola')
-console.log(firstWord)
+
 let secondWord = prompt("inserisci un'altra parola")
-console.log(firstWord)
 
 
 if (firstWord.length > secondWord.length) {
-    console.log('secondWord')
-    console.log('firstWord')
+    console.log(secondWord)
+    console.log(firstWord)
 
-} else {
-    console.log('firstWord')
-    console.log('secondWord')
+} else if (firstWord.length < secondWord.length) {
+    console.log(firstWord)
+    console.log(secondWord)
+
+} else if (firstWord.length == secondWord.length) {
+    console.log('le parole sono uguali')
 }
